d7eaec4762d0966316603b658b9bb5e2c479e9f0
pandorian <pandorian@TARS>
2025-01-05 14:26:15 +0530

    Add project files.

---
+++ quizer-desktop/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace quizer_desktop
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
